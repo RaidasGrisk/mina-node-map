aabbd85d6754e2d5db5f51a6ed9e3338d1cb2a42
raidas <mrraidas@gmail.com>
2024-10-07 14:57:13 +0300

    add another data api source

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import re
 import numpy as np
 import asyncio
 import aiohttp
+import json
+import requests
 
 
 # read data source file
@@ -14,22 +16,42 @@ pattern_ipv4 = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
 ips = re.findall(pattern_ipv4, data)
 ips = list(set(ips))
 
+def fetch_from_ipinfo():
+    async def fetch_all_ips(ip_list):
 
-async def fetch_all_ips(ip_list):
+        async def fetch_ip_data(ip, session):
+            async with session.get(f'https://ipinfo.io/{ip}/geo') as response:
+                return await response.json() if response.status == 200 else None
 
-    async def fetch_ip_data(ip, session):
-        async with session.get(f'https://ipinfo.io/{ip}/geo') as response:
-            return await response.json() if response.status == 200 else None
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=10)) as session:
+            tasks = [fetch_ip_data(ip, session) for ip in ip_list]
+            return await asyncio.gather(*tasks)
 
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=10)) as session:
-        tasks = [fetch_ip_data(ip, session) for ip in ip_list]
-        return await asyncio.gather(*tasks)
+    # fetch ip data
+    data = asyncio.run(fetch_all_ips(ips))
 
-# fetch ip data
-ips_data = asyncio.run(fetch_all_ips(ips))
+
+def fetch_from_ipapis():
+    data = []
+    for i in range(0, len(ips), 100):
+        print(i)
+        response = requests.post(
+            url='https://api.ipapi.is',
+            params={'key': '1baa2deebf363c56'},
+            json={'ips': ips[i:i+100]}
+        )
+        response_ = response.json()
+        for ip in response_.keys():
+            if isinstance(response_[ip], dict):
+                data.append(response_[ip])
+
+
+with open('data.json', 'w') as f:
+    json.dump(data, f)
 
 # map
-coord_data = [np.array(item['loc'].split(','), dtype=np.float64) for item in ips_data]
+coord_data = [np.array(item['loc'].split(','), dtype=np.float64) for item in data]
+coord_data = [(item['location']['latitude'], item['location']['longitude']) for item in data]
 
 mymap = folium.Map(location=[20.0, 0.0], zoom_start=2, max_zoom=1)
 folium.TileLayer('cartodbdark_matter').add_to(mymap)
